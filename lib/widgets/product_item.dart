e194b3c0e0060ef998a32006734319562fe88050
Ali Elahi <alielahi.1378@gmail.com>
2021-09-21 18:43:28 +0430

    Feat: ProductItem widget is created with fav and add to cart button

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,3 +1,4 @@
+import 'package:favorite_button/favorite_button.dart';
 import 'package:flutter/material.dart';
 
 class ProductItem extends StatelessWidget {
@@ -9,6 +10,28 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridTile(child: Image.network(imageUrl));
+    return GridTile(
+      child: Image.network(
+        imageUrl,
+        fit: BoxFit.cover,
+      ),
+      footer: GridTileBar(
+        backgroundColor: Colors.black54,
+        title: Text(
+          title,
+          textAlign: TextAlign.center,
+        ),
+        leading: FavoriteButton(
+          iconSize: 40.0,
+          valueChanged: (_isFavorite) {
+            print('Is Favorite $_isFavorite');
+          },
+        ),
+        trailing: IconButton(
+          color: Colors.white,
+          icon: Icon(Icons.add_shopping_cart_outlined),
+        ),
+      ),
+    );
   }
 }
