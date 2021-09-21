6d94ce0e8aa5c5101ed775fe9da74692b29b3c68
Ali Elahi <alielahi.1378@gmail.com>
2021-09-21 13:38:52 +0430

    Feat: GridView.builder and product_item widget are added to the project

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,8 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_app/models/product.dart';
+import 'package:shopping_app/widgets/product_item.dart';
 
 class ProductsOverviewScreen extends StatelessWidget {
+  List<Product> loadedProducts = [
+    Product(
+      id: 'p1',
+      title: 'Red Shirt',
+      description: 'A red shirt - it is pretty red!',
+      price: 29.99,
+      imageUrl:
+          'https://m.media-amazon.com/images/I/517Nhj617jL._AC_UX342_.jpg',
+    ),
+    Product(
+        id: 'p2',
+        title: 'Trousers',
+        description: 'A nice pair of trousers.',
+        price: 59.99,
+        imageUrl:
+            'https://ae01.alicdn.com/kf/Hbfdf157c02aa4b74b8a103b7f65e91861/Men-s-Classic-Striped-Dress-Trousers-Korean-Ankle-Length-Suit-Pants-Men-Business-Fomal-Office-Trousers.jpg_Q90.jpg_.webp'),
+    Product(
+      id: 'p3',
+      title: 'Yellow Scarf',
+      description: 'Warm and cozy - exactly what you need for the winter.',
+      price: 19.99,
+      imageUrl:
+          'https://n.nordstrommedia.com/id/sr3/8e626d44-ac02-4e28-b87b-6196f6dbaabf.jpeg?h=365&w=240&dpr=2',
+    ),
+    Product(
+      id: 'p4',
+      title: 'A Pan',
+      description: 'Prepare any meal you want.',
+      price: 49.99,
+      imageUrl:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('MyShop'),
+      ),
+      body: GridView.builder(
+        padding: EdgeInsets.all(10.0),
+        itemCount: loadedProducts.length,
+        itemBuilder: (context, i) => ProductItem(
+          id: loadedProducts[i].id,
+          title: loadedProducts[i].title,
+          imageUrl: loadedProducts[i].imageUrl,
+        ),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          mainAxisSpacing: 10.0,
+          crossAxisSpacing: 10.0,
+          childAspectRatio: 3 / 2,
+        ),
+      ),
+    );
   }
 }
